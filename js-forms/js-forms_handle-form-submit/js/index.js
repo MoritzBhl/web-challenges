5640849716b4ee03fb8c3b202d1d9e5a12f5f2da
Moritz Biehl <biehlmo@gmail.com>
2024-06-11 18:38:36 +0200

    Add Challenge Solution Form Submit

---
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,26 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const formElements = event.target.elements;
+//   const formData = new FormData(event.target);
+//   const data = Object.fromEntries(formData);
+//   event.target.reset();
+//   formElements.firstName.focus();
+// });
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  let badnessSum =
+    parseInt(formElements.age.value) + parseInt(formElements.badness.value);
+  console.log(
+    `The age-badness-sum of ${formElements.firstName.value} is "${badnessSum}"`
+  );
+  event.target.reset();
+  formElements.firstName.focus();
+});
