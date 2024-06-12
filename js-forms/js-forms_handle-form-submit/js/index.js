2fc2d483b1e4180cb6542e015370ed7700b32d33
Moritz Biehl <biehlmo@gmail.com>
2024-06-12 17:56:20 +0200

    Add Challenge Solution Form Submit

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -2,15 +2,6 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const formElements = event.target.elements;
-//   const formData = new FormData(event.target);
-//   const data = Object.fromEntries(formData);
-//   event.target.reset();
-//   formElements.firstName.focus();
-// });
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formElements = event.target.elements;
