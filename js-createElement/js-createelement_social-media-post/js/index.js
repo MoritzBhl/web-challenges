4fc154ebfe0a04e54d6d366c72c4935151ce5ae5
Moritz Biehl <biehlmo@gmail.com>
2024-06-12 11:42:35 +0200

    Add Challenge Solution Social Media Post

---
+++ js-createElement/js-createelement_social-media-post/js/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+function handleLikeButtonClick(event) {
+  const buttonElement = event.target;
+  buttonElement.classList.toggle("post__button--liked");
+}
+
+const likeButton = document.querySelector('[data-js="like-button"]');
+likeButton.addEventListener("click", handleLikeButtonClick);
+
+// Exercise:
+// Use document.createElement() and append another social media post to the body.
+
+const article = document.createElement("article");
+const p = document.createElement("p");
+const footer = document.createElement("footer");
+const span = document.createElement("span");
+const button = document.createElement("button");
+
+article.classList.add("post");
+p.classList.add("post__content");
+footer.classList.add("post__footer");
+span.classList.add("post__username");
+button.classList.add("post__button");
+
+p.textContent = "This is my self created article card";
+span.textContent = "@MoritzBHL";
+button.textContent = "♥ Like";
+
+article.append(p, footer);
+footer.append(span, button);
+document.body.append(article);
+
+button.addEventListener("click", handleLikeButtonClick);
