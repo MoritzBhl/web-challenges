1fd34b6f87452cb1e23947b29e27cb7d07750675
Moritz Biehl <biehlmo@gmail.com>
2024-06-14 16:51:35 +0200

    Add Challenge Solution Shapes

---
+++ js-structure/js-structure_shapes/index.js
@@ -0,0 +1,16 @@
+import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
+import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
+console.clear();
+
+const root = document.getElementById("root");
+
+const circleElement = Circle();
+root.append(circleElement);
+
+const squareElement = Square();
+root.append(squareElement);
+
+const pentagonElement = Pentagon();
+root.append(pentagonElement);
