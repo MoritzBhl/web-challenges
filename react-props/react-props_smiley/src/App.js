2681eb6a5f82c1f0d33332482b584f24c5917b0b
Moritz Biehl <biehlmo@gmail.com>
2024-06-21 18:35:39 +0200

    Add Challenge Solution Smiley

---
+++ react-props/react-props_smiley/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Smiley isHappy />
+      <Smiley />
+    </div>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return <h1>{isHappy ? `🥳` : `😭`}</h1>;
+}
