942349145db772f4840faa32a5e10b6821e45bab
Moritz Biehl <biehlmo@gmail.com>
2024-06-25 16:53:46 +0200

    Add Challenge Solution Table Reservation

---
+++ react-state-2/react-state-2_table-reservation/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+import Counter from "./components/Counter";
+import "./styles.css";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+  function handleAdd() {
+    setPeople(people + 1);
+  }
+  function handleSub() {
+    setPeople(people - 1);
+  }
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter onAdd={handleAdd} onSub={handleSub} />
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
